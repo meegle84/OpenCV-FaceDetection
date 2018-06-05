87cba7b1bd9e0e901d3a837e2735ee087a82547a
meegle84 <meegle84@gmail.com>
2018-06-05 09:17:40 +0200

    smile detection added

--- facedetector.py
+++ facedetector.py
@@ -1,19 +1,22 @@
 import numpy as np
 import cv2
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
 
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+# https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+# https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+# https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_smile.xml
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 cap = cv2.VideoCapture(1)
+cap.set(3, 640) #width
+cap.set(4, 480) #height
 
 while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 2, 5)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -23,6 +26,10 @@ while 1:
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        smile = smile_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in smile:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
 
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
